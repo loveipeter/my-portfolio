ea01b771cc704aa831b6a351acfef9274e396f5c
Peter Lovei <peter.lovei@philips.com>
2024-12-02 13:47:10 +0100

    Photo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={require('./img/pl.jpg')} width={"200px"} height={"200px"} />
         <p>Hello, I am Peter Lovei, PhD 🎉</p>
       </header>
     </div>
